5a699c02cc6e2947ee4bd25655f254e5d70a8608
Jose Emmanuel B. Laurel <jose.laurel@obf.ateneo.edu>
2023-05-12 22:21:15 +0800

    Began work on about and contact pages

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<div>
+    <h1>Hello</h1>
+    <p>Where you? This is a test...</p>
+</div>
+
+<style>
+    div {
+        position: relative;
+        margin-left: auto;
+		display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin: -225px 0 0 240px;
+        color: white;
+        line-height: 10%;
+    }
+</style>
